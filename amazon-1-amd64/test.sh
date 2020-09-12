47ff358d70e514c6967e850f9b1f56e4fef9cc61
Andrew Murray <radarhere@users.noreply.github.com>
2020-09-12 12:39:01 +1000

    run

--- amazon-1-amd64/test.sh
+++ amazon-1-amd64/test.sh
@@ -3,6 +3,5 @@ source /vpy3/bin/activate
 cd /Pillow
 export DISPLAY=:99.0
 export LD_LIBRARY_PATH=/usr/lib
-make clean
-make install-coverage
-/usr/bin/xvfb-run -a pytest -vx --cov PIL --cov-report term Tests
+python3 setup.py install
+python3 -c "import numpy as np;from PIL import Image;from PIL import ImageTk;import tkinter;root = tkinter.Tk();example_image_array = np.full((100,100), 123, dtype=np.uint8);example_image = Image.fromarray(example_image_array);example_image_tk = ImageTk.PhotoImage(image=example_image)"
