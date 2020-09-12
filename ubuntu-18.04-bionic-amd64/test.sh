d4f3819f04a5b0182ce6834e66b1f005cc72f46b
Andrew Murray <radarhere@users.noreply.github.com>
2020-09-12 20:50:32 +1000

    99

--- ubuntu-18.04-bionic-amd64/test.sh
+++ ubuntu-18.04-bionic-amd64/test.sh
@@ -2,6 +2,7 @@
 source /vpy3/bin/activate
 cd /Pillow
 make clean
+export DISPLAY=:99.0
 python3 -m pip install numpy
 python3 setup.py install
 python3 -c "import numpy as np;from PIL import Image;from PIL import ImageTk;import tkinter;root = tkinter.Tk();example_image_array = np.full((100,100), 123, dtype=np.uint8);example_image = Image.fromarray(example_image_array);example_image_tk = ImageTk.PhotoImage(image=example_image)"
